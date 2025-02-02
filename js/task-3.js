99880d8ba44774f23189ec82ca9a00d670e9a647
letonzt <letonzt@gmail.com>
2025-02-02 21:26:09 +0100

    HomeWork-JS-8

--- js/task-3.js
+++
@@ -1,5 +0,0 @@
-const textInput = document.querySelector('input#name-input');
-const output = document.querySelector('span#name-output');
-textInput.addEventListener("input", (event) => {
-    const inputValue = event.currentTarget.value.trim();
-        output.textContent = inputValue.length>0 ? inputValue : "Anonymous"; });
